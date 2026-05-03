7fb86e634c4ab722cc90f2ce0f6e5e97c99f22ed
crazyprash007 <prashantkr.sharma07@gmail.com>
2026-05-03 19:30:00 +0530

    fix(db): remove redundant CREATE USER/DATABASE from init.sql to fix docker startup error

--- docker/init.sql
+++ docker/init.sql
@@ -1,6 +1,10 @@
 -- docker/init.sql
-CREATE USER faceuser WITH PASSWORD 'strongpassword';
-CREATE DATABASE facesdb OWNER faceuser;
+-- The 'faceuser' and 'facesdb' are automatically created by the postgres Docker image 
+-- using the POSTGRES_USER and POSTGRES_DB environment variables. 
+
+-- Apply least-privilege grants
+-- (Note: faceuser is the db owner because of POSTGRES_USER, so it has implicit rights, 
+-- but these grants enforce the read/write expectations from the design).
 GRANT CONNECT ON DATABASE facesdb TO faceuser;
 GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public TO faceuser;
 -- No UPDATE, DELETE, DROP privileges
